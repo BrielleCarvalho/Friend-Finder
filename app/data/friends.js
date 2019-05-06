c724bf125ccad1f8d495beb2c69b21b5e31bdf12
BrielleCarvalho <brielle.127@gmail.com>
2019-05-06 10:38:48 -0500

    final commit

---
+++ app/data/friends.js
@@ -0,0 +1,149 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Patti Mayonnaise',
+		photo: 'https://i.ytimg.com/vi/TUTg6fNgC0I/hqdefault.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Helga G. Pataki',
+		photo: 'https://pbs.twimg.com/profile_images/978134387970555905/agRbq44P_400x400.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Angelica Pickles',
+		photo: 'https://i.pinimg.com/236x/0a/a9/12/0aa91211d5791ab3e4ee8af11879b87d--rugrats-costume-pickles.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Daria',
+		photo: 'http://forreadingaddicts.co.uk/wp-content/uploads/2017/05/daria-ad-book.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Debbie Thornberry',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMTQ3NTY0MDk0Nl5BMl5BanBnXkFtZTgwNjM4MzgzMjE@._V1_UY268_CR87,0,182,268_AL_.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Mrs. Piggy',
+		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Reggie Rocket',
+		photo: 'https://pbs.twimg.com/profile_images/729074023246798848/TXBZqUm6_400x400.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Ashley Spinelli',
+		photo: 'https://data.whicdn.com/images/182867263/large.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Sailor Moon',
+		photo: 'https://cdn-images-1.medium.com/max/788/1*EH0VB2ljcBAu7XXWd732Fw.jpeg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	}
+];
+
+module.exports = friendsArray;
